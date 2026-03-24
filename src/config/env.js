14c6294bbafef754e2e86ff4c3b855d9120b1706
xGL24x-Dev <gustavolizarazochanfuelan@gmail.com>
2026-03-24 13:58:49 -0500

    estructura base del proyecto

---
+++ src/config/env.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const ENV = {
+    PORT: process.env.PORT || 3000,
+    JWT_SECRET: process.env.JWT_SECRET,
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_NAME: process.env.DB_NAME,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+}
